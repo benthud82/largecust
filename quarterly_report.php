1784ceda3f53ee3fda13c29aab5aaed0153b79f0
Bentley <bentley.hudson@henryschein.com>
2019-08-20 08:58:11 -0500

    daily commit

--- quarterly_report.php
+++ quarterly_report.php
@@ -1,77 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
+<?php
+//set page headers
+$page_title = "Admin - Qtr Report";
+include 'layout_header.php';
+include_once 'config/database.php';
+include_once 'objects/reportoptions.php';
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
 
-        <title>Admin - Qtr Report</title>
+//pass connection to Report class
+$product = new Report($db);
 
-        <!-- Latest compiled and minified Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+//call read method in Report class
+$stmt = $product->read();
+?>
 
-        <!-- our custom CSS -->
-        <link rel="stylesheet" href="libs/css/custom.css" />
-
-    </head>
-    <body>
-
-        <!-- container -->
-        <div class="container-fluid">
-
+<!--sidebar checkboxes-->
+<div class="row">
+    <div class="col-sm-3 ">
+        <p class="h3"><strong>Select Reports to Include:</strong></p>
+        <form>
             <?php
-            include_once 'config/database.php';
-            include_once 'objects/reportoptions.php';
-
-            // get database connection
-            $database = new Database();
-            $db = $database->getConnection();
-
-            //pass connection to Report class
-            $product = new Report($db);
-
-            //call read method in Report class
-            $stmt = $product->read();
-            ?>
-
-            <!--sidebar checkboxes-->
-            <div class="col-sm-3 ">
-                <p class="h3"><strong>Select Reports to Include:</strong></p>
-                <form>
-                    <?php
-                    //loop through and create checkbox options
-                    while ($row_category = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        extract($row_category);
-                        echo " <label style='font-size:12px;'>
+            //loop through and create checkbox options
+            while ($row_category = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                extract($row_category);
+                echo " <label style='font-size:12px;'>
                                         <input type='checkbox' name='rec-type' value='main' id='$report_id'/> $report_title</label>
                                      <br>";
-                    }
-                    ?>
-                </form>
-            </div>
+            }
+            ?>
+        </form>
+    </div>
 
-            <!--display of sample reports-->
-            <div class="col-sm-9 border_separator_left">
-                <div class="text_jpeg_group">
-                    <div class="row">
-                        <div class="col-md-5">
-                            <div class="in-middle">
-                                <h1 style="text-align: right;">Fully Automated Dashboard</h1>
-                                <p style="text-align: right;">Utilizing your data, dashboard updates based on your timing to provide real-time trends and results.</p>
-                            </div>
-                        </div>
-                        <div class="col-md-7">
-                            <img src="images/cilantrolimechicken.jpg" style="width:100%;">
-                        </div>
+    <!--display of sample reports-->
+    <div class="col-sm-9 border_separator_left">
+        <div class="text_jpeg_group">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="in-middle">
+                        <h1 style="text-align: right;">Fully Automated Dashboard</h1>
+                        <p style="text-align: right;">Utilizing your data, dashboard updates based on your timing to provide real-time trends and results.</p>
                     </div>
                 </div>
+                <div class="col-md-6">
+                    <img src="images/smashedpotatoes.jpg" style="width:100%;">
+                </div>
             </div>
-
-
-
-            <!--containter-->
         </div>
+    </div>
+</div>
 
-    </body>
-</html>
+<?php
+// footer
+include_once "layout_footer.php";
+?>
